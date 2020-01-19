fddf54913345f1f50ce0bd4600110120a2052e4a
epinhasovich <57751187+epinhasovich@users.noreply.github.com>
2020-01-19 16:19:46 +0200

    cleaned up format of data and added registration stats

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request, url_for, send_from_directory
+from flask import Flask, render_template, request, url_for, send_from_directory
 import threading
 from datetime import datetime
 
@@ -10,8 +10,10 @@ students = [
         "id": 1,
         "first_name": "Eric",
         "last_name": "Pinhasovich",
-        "existing_magic_skills": [{"skill": 'Healing',
-                                   "level:": 4}],
+        "existing_magic_skills": [
+                            {"skill": 'Healing', "level": 4},
+                            {"skill": 'Animation', "level": 2}
+                                ],
         "desired_magic_skills": ["Poison"],
         "courses": "Magic For Day-to-Day Life",
         "created_time": datetime.now(),
@@ -42,6 +44,34 @@ students = [
                                  ],
         "desired_magic_skills": ["Invisibility", "Immortality"],
         "courses": "Magic For Medical Professionals",
+        "created_time": datetime.now(),
+        "updated_time": ''
+    },
+    {
+        "id": 4,
+        "first_name": "Draco",
+        "last_name": "Malfoy",
+        "existing_magic_skills": [
+                            {"skill": 'Poison', "level": 4},
+                            {'skill': 'Possesion', 'level': 4},
+                            {'skill': 'Immortality', 'level': 5}
+                                 ],
+        "desired_magic_skills": ["Summoning", "Animation", "Healing"],
+        "courses": "Alchemy Basics",
+        "created_time": datetime.now(),
+        "updated_time": ''
+    },
+    {
+        "id": 5,
+        "first_name": "Neville",
+        "last_name": "Longbottom",
+        "existing_magic_skills": [
+                            {"skill": 'Invisibility', "level": 1},
+                            {'skill': 'Illusion', 'level': 1},
+                            {'skill': 'Elemental', 'level': 1}
+                                 ],
+        "desired_magic_skills": ["Summoning", "Disintegration", "Healing"],
+        "courses": "Dating With Magic",
         "created_time": datetime.now(),
         "updated_time": ''
     }
@@ -105,9 +135,7 @@ def create_student():
         "courses": course_list(),
         "created_time": datetime.now(),
         "updated_time": ''
-
     }
-
     students.append(student)
     return render_template("student.html", student=student)
 
@@ -120,12 +148,16 @@ def edit_student(id):
     elif request.method == 'POST':
         fname = request.form.get('firstForm')
         lname = request.form.get('lastForm')
-        student_existing_magic_skills = request.form.getlist("magic_skills[]")
         student_wanted_magic_skills = request.form.getlist("wanted_skills[]")
+        magic_skillz = request.form.getlist('magic_skills[]')
+        magic_skillz = [{'skill': skill} for skill in magic_skillz]
+        magic_level = request.form.getlist('magic_level[]')
+        for i in range(len(magic_skillz)):
+            magic_skillz[i]['level'] = magic_level[i]
         classes = request.form.getlist("classes[]")
         student['first_name'] = fname
         student['last_name'] = lname
-        student["existing_magic_skills"] = student_existing_magic_skills
+        student["existing_magic_skills"] = magic_skillz
         student["desired_magic_skills"] = student_wanted_magic_skills
         student["courses"] = classes
         student['updated_time'] = datetime.now()
@@ -137,6 +169,9 @@ def pie_chart():
     data = students
     existing_skills = []
     desired_skills = []
+    student_number = len(students)
+    student_count = [student_number][0]
+    print(student_count)
     student = [student for student in data]
     for skill in student:
         e_skills = skill['existing_magic_skills']
@@ -156,8 +191,14 @@ def pie_chart():
         count = desired_skills.count(skill)
         if count:
             desired_list.append({'y': count, 'label': skill})
-            print(desired_list)
-    return render_template('pie-chart.html', data=data, existing_list=existing_list, desired_list=desired_list)
+    return render_template('pie-chart.html', data=data, existing_list=existing_list, desired_list=desired_list, student_count=student_count)
+
+
+# @app.route('/students/<int:id>/delete', methods=['POST'])
+# def delete_student(id):
+#     student = [student for student in students if student['id'] == id][0]
+#     students.remove(student[0])
+#     return render_template("students.html", students=students)
 
 
 if __name__ == "__main__":
